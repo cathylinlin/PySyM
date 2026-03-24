2220f3822e4cf844df13a3dd46e8f50910530233
cathylinlin <bluejam001@163.com>
2026-03-24 20:44:14 +0800

    Add files via upload

--- src/PySyM/core/representation/__init__.py
+++ src/PySyM/core/representation/__init__.py
@@ -1,1 +1,13 @@
+from .abstract_representation import GroupRepresentation
+from .character import Character
+from .matrix_representation import MatrixRepresentation
+from .induced import InducedRepresentation
+from .irreducible import IrreducibleRepresentationFinder
 
+__all__ = [
+    'GroupRepresentation',
+    'Character',
+    'MatrixRepresentation',
+    'InducedRepresentation',
+    'IrreducibleRepresentationFinder'
+]
